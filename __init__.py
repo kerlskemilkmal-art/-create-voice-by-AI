54e2c1543b06320505b64893ed3d8be75386acfe
kerlskemilkmal-art <kerlskemilkmal@gmail.com>
2026-06-05 17:02:55 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,192 +1,151 @@
-"""Score functions, performance metrics, pairwise metrics and distance computations."""
+"""Configure global settings and get information about the working environment."""
 
 # Authors: The scikit-learn developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-from sklearn.metrics import cluster
-from sklearn.metrics._classification import (
-    accuracy_score,
-    balanced_accuracy_score,
-    brier_score_loss,
-    class_likelihood_ratios,
-    classification_report,
-    cohen_kappa_score,
-    confusion_matrix,
-    d2_brier_score,
-    d2_log_loss_score,
-    f1_score,
-    fbeta_score,
-    hamming_loss,
-    hinge_loss,
-    jaccard_score,
-    log_loss,
-    matthews_corrcoef,
-    multilabel_confusion_matrix,
-    precision_recall_fscore_support,
-    precision_score,
-    recall_score,
-    zero_one_loss,
-)
-from sklearn.metrics._dist_metrics import DistanceMetric
-from sklearn.metrics._plot.confusion_matrix import ConfusionMatrixDisplay
-from sklearn.metrics._plot.det_curve import DetCurveDisplay
-from sklearn.metrics._plot.precision_recall_curve import PrecisionRecallDisplay
-from sklearn.metrics._plot.regression import PredictionErrorDisplay
-from sklearn.metrics._plot.roc_curve import RocCurveDisplay
-from sklearn.metrics._ranking import (
-    auc,
-    average_precision_score,
-    confusion_matrix_at_thresholds,
-    coverage_error,
-    dcg_score,
-    det_curve,
-    label_ranking_average_precision_score,
-    label_ranking_loss,
-    metric_at_thresholds,
-    ndcg_score,
-    precision_recall_curve,
-    roc_auc_score,
-    roc_curve,
-    top_k_accuracy_score,
-)
-from sklearn.metrics._regression import (
-    d2_absolute_error_score,
-    d2_pinball_score,
-    d2_tweedie_score,
-    explained_variance_score,
-    max_error,
-    mean_absolute_error,
-    mean_absolute_percentage_error,
-    mean_gamma_deviance,
-    mean_pinball_loss,
-    mean_poisson_deviance,
-    mean_squared_error,
-    mean_squared_log_error,
-    mean_tweedie_deviance,
-    median_absolute_error,
-    r2_score,
-    root_mean_squared_error,
-    root_mean_squared_log_error,
-)
-from sklearn.metrics._scorer import (
-    check_scoring,
-    get_scorer,
-    get_scorer_names,
-    make_scorer,
-)
-from sklearn.metrics.cluster import (
-    adjusted_mutual_info_score,
-    adjusted_rand_score,
-    calinski_harabasz_score,
-    completeness_score,
-    consensus_score,
-    davies_bouldin_score,
-    fowlkes_mallows_score,
-    homogeneity_completeness_v_measure,
-    homogeneity_score,
-    mutual_info_score,
-    normalized_mutual_info_score,
-    pair_confusion_matrix,
-    rand_score,
-    silhouette_samples,
-    silhouette_score,
-    v_measure_score,
-)
-from sklearn.metrics.pairwise import (
-    euclidean_distances,
-    nan_euclidean_distances,
-    pairwise_distances,
-    pairwise_distances_argmin,
-    pairwise_distances_argmin_min,
-    pairwise_distances_chunked,
-    pairwise_kernels,
-)
+# Machine learning module for Python
+# ==================================
+#
+# sklearn is a Python module integrating classical machine
+# learning algorithms in the tightly-knit world of scientific Python
+# packages (numpy, scipy, matplotlib).
+#
+# It aims to provide simple and efficient solutions to learning problems
+# that are accessible to everybody and reusable in various contexts:
+# machine-learning as a versatile tool for science and engineering.
+#
+# See https://scikit-learn.org for complete documentation.
 
-__all__ = [
-    "ConfusionMatrixDisplay",
-    "DetCurveDisplay",
-    "DistanceMetric",
-    "PrecisionRecallDisplay",
-    "PredictionErrorDisplay",
-    "RocCurveDisplay",
-    "accuracy_score",
-    "adjusted_mutual_info_score",
-    "adjusted_rand_score",
-    "auc",
-    "average_precision_score",
-    "balanced_accuracy_score",
-    "brier_score_loss",
-    "calinski_harabasz_score",
-    "check_scoring",
-    "class_likelihood_ratios",
-    "classification_report",
+import importlib as _importlib
+import logging
+import os
+import random
+
+from sklearn._config import config_context, get_config, set_config
+
+logger = logging.getLogger(__name__)
+
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.Y.ZaN   # Alpha release
+#   X.Y.ZbN   # Beta release
+#   X.Y.ZrcN  # Release Candidate
+#   X.Y.Z     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = "1.9.0"
+
+
+# On OSX, we can get a runtime error due to multiple OpenMP libraries loaded
+# simultaneously. This can happen for instance when calling BLAS inside a
+# prange. Setting the following environment variable allows multiple OpenMP
+# libraries to be loaded. It should not degrade performances since we manually
+# take care of potential over-subscription performance issues, in sections of
+# the code where nested OpenMP loops can happen, by dynamically reconfiguring
+# the inner OpenMP runtime to temporarily disable it while under the scope of
+# the outer OpenMP parallel section.
+os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "True")
+
+# Workaround issue discovered in intel-openmp 2019.5:
+# https://github.com/ContinuumIO/anaconda-issues/issues/11294
+os.environ.setdefault("KMP_INIT_AT_FORK", "FALSE")
+
+# `_distributor_init` allows distributors to run custom init code.
+# For instance, for the Windows wheel, this is used to pre-load the
+# vcomp shared library runtime for OpenMP embedded in the sklearn/.libs
+# sub-folder.
+# It is necessary to do this prior to importing show_versions as the
+# later is linked to the OpenMP runtime to make it possible to introspect
+# it and importing it first would fail if the OpenMP dll cannot be found.
+from sklearn import __check_build, _distributor_init  # noqa: E402 F401
+from sklearn.base import clone  # noqa: E402
+from sklearn.utils._show_versions import show_versions  # noqa: E402
+
+_submodules = [
+    "calibration",
+    "callback",
     "cluster",
-    "cohen_kappa_score",
-    "completeness_score",
-    "confusion_matrix",
-    "confusion_matrix_at_thresholds",
-    "consensus_score",
-    "coverage_error",
-    "d2_absolute_error_score",
-    "d2_brier_score",
-    "d2_log_loss_score",
-    "d2_pinball_score",
-    "d2_tweedie_score",
-    "davies_bouldin_score",
-    "dcg_score",
-    "det_curve",
-    "euclidean_distances",
-    "explained_variance_score",
-    "f1_score",
-    "fbeta_score",
-    "fowlkes_mallows_score",
-    "get_scorer",
-    "get_scorer_names",
-    "hamming_loss",
-    "hinge_loss",
-    "homogeneity_completeness_v_measure",
-    "homogeneity_score",
-    "jaccard_score",
-    "label_ranking_average_precision_score",
-    "label_ranking_loss",
-    "log_loss",
-    "make_scorer",
-    "matthews_corrcoef",
-    "max_error",
-    "mean_absolute_error",
-    "mean_absolute_percentage_error",
-    "mean_gamma_deviance",
-    "mean_pinball_loss",
-    "mean_poisson_deviance",
-    "mean_squared_error",
-    "mean_squared_log_error",
-    "mean_tweedie_deviance",
-    "median_absolute_error",
-    "metric_at_thresholds",
-    "multilabel_confusion_matrix",
-    "mutual_info_score",
-    "nan_euclidean_distances",
-    "ndcg_score",
-    "normalized_mutual_info_score",
-    "pair_confusion_matrix",
-    "pairwise_distances",
-    "pairwise_distances_argmin",
-    "pairwise_distances_argmin_min",
-    "pairwise_distances_chunked",
-    "pairwise_kernels",
-    "precision_recall_curve",
-    "precision_recall_fscore_support",
-    "precision_score",
-    "r2_score",
-    "rand_score",
-    "recall_score",
-    "roc_auc_score",
-    "roc_curve",
-    "root_mean_squared_error",
-    "root_mean_squared_log_error",
-    "silhouette_samples",
-    "silhouette_score",
-    "top_k_accuracy_score",
-    "v_measure_score",
-    "zero_one_loss",
+    "covariance",
+    "cross_decomposition",
+    "datasets",
+    "decomposition",
+    "dummy",
+    "ensemble",
+    "exceptions",
+    "experimental",
+    "externals",
+    "feature_extraction",
+    "feature_selection",
+    "frozen",
+    "gaussian_process",
+    "inspection",
+    "isotonic",
+    "kernel_approximation",
+    "kernel_ridge",
+    "linear_model",
+    "manifold",
+    "metrics",
+    "mixture",
+    "model_selection",
+    "multiclass",
+    "multioutput",
+    "naive_bayes",
+    "neighbors",
+    "neural_network",
+    "pipeline",
+    "preprocessing",
+    "random_projection",
+    "semi_supervised",
+    "svm",
+    "tree",
+    "discriminant_analysis",
+    "impute",
+    "compose",
 ]
+
+__all__ = _submodules + [
+    # Non-modules:
+    "clone",
+    "get_config",
+    "set_config",
+    "config_context",
+    "show_versions",
+]
+
+
+def __dir__():
+    return __all__
+
+
+def __getattr__(name):
+    if name in _submodules:
+        return _importlib.import_module(f"sklearn.{name}")
+    else:
+        try:
+            return globals()[name]
+        except KeyError:
+            raise AttributeError(f"Module 'sklearn' has no attribute '{name}'")
+
+
+def setup_module(module):
+    """Fixture for the tests to assure globally controllable seeding of RNGs"""
+
+    import numpy as np
+
+    # Check if a random seed exists in the environment, if not create one.
+    _random_seed = os.environ.get("SKLEARN_SEED", None)
+    if _random_seed is None:
+        _random_seed = np.random.uniform() * np.iinfo(np.int32).max
+    _random_seed = int(_random_seed)
+    print("I: Seeding RNGs with %r" % _random_seed)
+    np.random.seed(_random_seed)
+    random.seed(_random_seed)
